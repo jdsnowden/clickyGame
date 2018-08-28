0443d12715005c34355116b279012ff5700bf4cb
Your Name <jeffrey_snowden@me.com>
2018-08-28 10:17:28 -0700

    final push

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
-import Cards from "./components/cards"
+import React from "react";
+import Game from "./components/Game";
 
-
-class App extends Component {
-
-
-
-
-
-
-
-
-
-
-
-  render() {
-    return (
-      <div className="App">
-      <Cards>
-
-      </Cards>
-      </div>
-    );
-  }
-}
+const App = () => <Game />;
 
 export default App;
